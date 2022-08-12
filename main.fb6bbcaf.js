aac35977f67e6467a5ce2cba20ddc12768af51d1
Ryan Miles <ryanm1114@gmail.com>
2022-08-12 14:11:40 -0600

    Updates

--- main.fb6bbcaf.js
+++ main.fb6bbcaf.js
@@ -6435,23 +6435,13 @@ Object.defineProperty(exports, "__esModule", {
 
 __export(require("./CSV"));
 },{"./CSV":"node_modules/csv-string/dist/CSV.js"}],"js/main.js":[function(require,module,exports) {
-"use strict";
-
-var CSV = _interopRequireWildcard(require("csv-string"));
-
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-CSV = (require('csv-string'), function () {
-  throw new Error('"' + "CSV" + '" is read-only.');
-}());
+CSV = require('csv-string');
 /**
  * Shuffles array in place. ES6 version
  * @param {Array} a items An array containing the items.
